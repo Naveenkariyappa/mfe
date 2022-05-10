da2aef64a79866aaed23c2d6f74924372b1122c1
Naveen Kariyappa <naveenkariyappa@gmail.com>
2022-05-10 21:38:04 +0530

    class name collusion

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,15 +2,24 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 
 const App = () => {
+  const generateClassName = createGenerateClassName({
+    productionPrefix: 'con',
+  });
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />;
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp />;
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
 
